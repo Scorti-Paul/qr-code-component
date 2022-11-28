a58ce88577455e88e8ac362e758a6d71915ba043
Scorti-Paul <scortip@gmail.com>
2022-11-28 17:42:15 +0000

    completed

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import qrCode from './assets/image-qr-code.png'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <main className='w-full h-screen p-4 flex items-center justify-center bg-lightgray'>
+      <div className='bg-white max-w-[23rem] p-4 rounded-3xl shadow-md'>
+        <div className='rounded-xl'>
+          <img className='rounded-xl' src={qrCode} alt="qr-code" />
+        </div>
+        <div className='text-center'>
+          <h1 className='text-2xl font-bold py-6 px-4'>Improve your front-end skills by building projects</h1>
+          <p className='text-grayishblue px-10 pb-6'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     screens: {
       sm: '375px',
       lg: '1440px'
     },
-    colors: {
-      white: 'hsl(0, 0%, 100%)',
-      grayishblue: 'hsl(220, 15%, 55%)',
-      lightgray: 'hsl(212, 45%, 89%)',
-      darkblue: 'hsl(218, 44%, 22%)',
+
+    extend: {
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        grayishblue: 'hsl(220, 15%, 55%)',
+        lightgray: 'hsl(212, 45%, 89%)',
+        darkblue: 'hsl(218, 44%, 22%)',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
