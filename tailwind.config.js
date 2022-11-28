86cdf01c788295ec39a6e4167add01e2ba6edc0a
Scorti-Paul <scortip@gmail.com>
2022-11-28 14:23:08 +0000

    structured project files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      sm: '375px',
+      lg: '1440px'
+    },
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      grayishblue: 'hsl(220, 15%, 55%)',
+      lightgray: 'hsl(212, 45%, 89%)',
+      darkblue: 'hsl(218, 44%, 22%)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
